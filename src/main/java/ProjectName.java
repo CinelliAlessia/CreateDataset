3ba820f85ee6b56590d9c18bd3c9c87c7aa68c73
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-30 23:12:02 +0200

    Creazione enum per poter assegnare nomi progetti corretti ed eccezione se cosi non fosse

---
+++ src/main/java/ProjectName.java
@@ -0,0 +1,4 @@
+public enum ProjectName {
+    BOOKKEEPER,
+    ZOOKEEPER
+}
---
+++ src/main/java/exception/NameProjectError.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class NameProjectError extends Throwable {
+    public NameProjectError(String message) {
+        super(message);
+    }
+}
